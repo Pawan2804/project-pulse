2422f4c18ac43d468f650371d2ba2463549c421a
Pawan2804 <pawan.vijayaraghavan98@gmail.com>
2024-07-30 20:44:18 +0530

    Initial Commit

---
+++ src/test/java/com/proj_management/project_pulse/ProjectPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj_management.project_pulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
